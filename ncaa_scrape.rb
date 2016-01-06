67a0d07bdc7eb5bc75c59a2a1ba9035e9b065918
Jen Trudell <jtrudell@gmail.com>
2016-01-06 12:54:43 -0600

    rubocop related refactoring

--- ncaa_scrape.rb
+++ ncaa_scrape.rb
@@ -5,46 +5,50 @@ class NCAABasketball
   attr_reader :division_one_data
 
   def initialize
-    @rankings_page = Nokogiri::HTML(open("http://www.ncaa.com/rankings/basketball-men/d1/ncaa-mens-basketball-rpi"))
+    @rankings_page = Nokogiri::HTML(open('http://www.ncaa.com/rankings/basketball-men/d1/ncaa-mens-basketball-rpi'))
     @division_one_data = team_names.zip(team_conferences, team_records)
+  end
+
+  def css_attribute(column)
+    ".ncaa-rankings-table > tbody > tr > td[#{column}]"
   end
 
   def team_names
     names = []
-    @rankings_page.css('.ncaa-rankings-table > tbody > tr > td[3]').each { |row| names << row.content }
+    @rankings_page.css(css_attribute(3)).each { |row| names << row.content }
     names
   end
 
   def team_conferences
     conferences = []
-    @rankings_page.css('.ncaa-rankings-table > tbody > tr > td[4]').each { |row| conferences << row.content }
+    @rankings_page.css(css_attribute(4)).each { |row| conferences << row.content }
     conferences
   end
 
   def team_records
     records = []
-    @rankings_page.css('.ncaa-rankings-table > tbody > tr > td[5]').each { |row| records << row.content }
+    @rankings_page.css(css_attribute(5)).each { |row| records << row.content }
     records
   end
 
   def team_rankings
-    rankings = Hash.new
-    @division_one_data.each_with_index { |team, index| rankings[index+1] = team }
+    rankings = {}
+    @division_one_data.each_with_index { |team, index| rankings[index + 1] = team }
     rankings
   end
 
   def team_record(team_name)
-    team = @division_one_data.select { |team| team.include?(team_name) }
-    team.flatten.last
+    selected_team = @division_one_data.select { |team| team.include?(team_name) }
+    selected_team.flatten.last
   end
 
   def team_wins(team_name)
-    team_record(team_name).partition("-").first.to_i
+    team_record(team_name).partition('-').first.to_i
   end
 
   def pick_five_total(*teams)
     teams.inject(0) do |total, team|
-      total += team_wins(team)
+      total + team_wins(team)
     end
   end
 end
